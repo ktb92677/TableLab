977f6439af0022f1cf8a745edd18adc278351ce1
kevdog <ktb92677@yahoo.com>
2016-10-15 10:30:14 -0700

    Added TissueBox.h class

---
+++ TissueBox.h
@@ -0,0 +1,32 @@
+#ifndef TISSUEBOX_
+#define TISSUEBOX_
+
+#include <iostream>
+#include <string>
+#include "Dimension.h"
+#include "Table.h"
+
+
+class TissueBox (Table *table){
+	private:
+		Dimension dim;
+		int current_num_tissues;
+		int max_num_tissues;
+	public:
+		TissueBox(Table *table){
+			cout << "What are the dimensions of your tissue box?"<< endl;
+			cout << "Length: ";
+			cin >> dim.l;
+			cout << "Width: ";
+			cin >> dim.w;
+
+			Table -> available_surface_area -= dim.l * dim.w;
+
+			cout << "How many tissues does your tissue box currently have?"
+			cin >> current_num_tissues;
+			cout << "How many tissues can your tissue box hold?"
+			cin >> max_num_tissues;
+		}
+};
+
+#endif
