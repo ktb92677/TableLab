7bda773cda1f002de605e4725d73b84b8b8e6373
kevdog <ktb92677@yahoo.com>
2016-10-15 09:31:19 -0700

    test commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 	cout << "hello world!" << endl;
-	cout << "antoher statement" < endl;
+	cout << "antoher statement" << endl;
+	cout << "one more statement" << endl;
 	return 0;
 }
